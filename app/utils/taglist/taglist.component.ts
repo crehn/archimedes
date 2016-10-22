877a937ed9f0b9aee8d82fd0122930b98958ab9d
Christian Rehn <mail@christian-rehn.de>
2016-10-22 18:04:35 +0200

    added computed tag colors

--- app/utils/taglist/taglist.component.ts
+++ app/utils/taglist/taglist.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChildren } from '@angular/core';
 
 @Component({
     selector: 'arch-taglist',
@@ -11,11 +11,33 @@ import { Component, Input } from '@angular/core';
     <div>
         <label for="tags">Tags:</label>
         <span class="tags">
-            <span class="sip-tag label label-default" *ngFor="let tag of tags">{{tag}}</span>
+            <span #tagSpan class="sip-tag label label-default" *ngFor="let tag of tags">{{tag}}</span>
         </span>
     </div>
     `
 })
-export class TagListComponent {
+export class TagListComponent implements AfterViewInit {
     @Input() tags: string[];
+
+    @ViewChildren('tagSpan')
+    tagSpans: ElementRef[];
+
+    ngAfterViewInit() {
+        this.tagSpans
+            .map(span => <HTMLElement>span.nativeElement)
+            .forEach(span => span.style.backgroundColor = this.getColor(span));
+    }
+
+    private getColor(span: HTMLElement): string {
+        let tagName = span.innerText;
+        let hash = 0;
+        for (let char of tagName) {
+            /* tslint:disable */
+            hash = ((hash << 5) - hash) + char.charCodeAt(0);
+            hash = hash & hash; // Convert to 32bit integer
+            /* tslint:enable */
+        }
+        let hashString = hash.toString(16);
+        return '#' + hashString + '000000'.substring(hashString.length);
+    }
 }
